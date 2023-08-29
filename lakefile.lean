56d126a2fb2974e07491f1aa399292f33634fda2
jt496 <john.talbot@gmail.com>
2023-08-29 16:28:45 +0100

    start

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «aES_Brandt» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «AESBrandt» {
+  -- add any library configuration options here
+}
