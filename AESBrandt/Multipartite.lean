b6199fb5bd2e2291f9611abaa2bc7814399d232e
jt496 <john.talbot@gmail.com>
2023-09-05 16:09:40 +0100

    started chromatic Number work

--- AESBrandt/Multipartite.lean
+++ AESBrandt/Multipartite.lean
@@ -8,6 +8,24 @@ open BigOperators
 namespace SimpleGraph
 variable {α β: Type _} {G : SimpleGraph α} [Fintype α] [Fintype (Sym2 α)] [DecidableEq α][Fintype β] [DecidableEq β] [DecidableRel G.Adj]
 
+lemma not_col_of_chrom_succ (h : G.chromaticNumber = k + 1) : ¬ G.Colorable k:=
+by 
+  sorry
+
+
+lemma chromatic_succ_verts (h : G.chromaticNumber = k + 1) (C : G.Coloring (Fin (k+1))) : ∀ i, ∃ v, C v = i :=
+by
+  by_contra hc
+  push_neg at hc
+  apply G.not_col_of_chrom_succ h
+  sorry
+
+
+lemma chromatic_succ_edges (h : G.chromaticNumber = k + 1) (C : G.Coloring (Fin (k+1))) : ∀ i j, i ≠ j →  ∃ u v, C u = i ∧ C v = j ∧ G.Adj u v:=
+by
+  sorry
+
+
 def CompleteMulti (G : SimpleGraph α) : Prop := Transitive (fun u v => ¬G.Adj u v)
 
 def CompleteKPartite  (G : SimpleGraph α) (k : ℕ): Prop := G.CompleteMulti ∧ G.chromaticNumber = k
