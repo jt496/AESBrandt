6442810aaf528dd0a4f9472ceae71bd47781d31b
jt496 <john.talbot@gmail.com>
2023-09-24 14:26:52 +0100

    added build file

--- AESBrandt.lean
+++ AESBrandt.lean
@@ -1,1 +1,6 @@
-def hello := "world"
+import AESBrandt.Chromatic
+import AESBrandt.Clique
+import AESBrandt.MaxCliqueFree
+import AESBrandt.CompleteMultiPartite
+import AESBrandt.P3bar
+import AESBrandt.Subgraphs
