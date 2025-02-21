8bfe82ef8768a45c4fc10db75724a87683dbe892
jt496 <john.talbot@gmail.com>
2025-02-21 12:59:10 +0000

    docs

--- AESBrandt/Finite.lean
+++ AESBrandt/Finite.lean
@@ -4,7 +4,7 @@ namespace SimpleGraph
 variable {V : Type*} {G : SimpleGraph V}
 
 variable [Fintype V]
-/--If V is finite and `P G` holds then there exists a maximal supergraph H of G
+/--If `V` is finite and `P G` holds then there exists a maximal supergraph `H` of `G`
 for which `P H` holds. -/
 lemma exists_maximal_supergraph (P : SimpleGraph V → Prop) (hG : P G) :
     ∃ H, G ≤ H ∧ Maximal P H := by
@@ -21,3 +21,4 @@ lemma exists_maximal_supergraph (P : SimpleGraph V → Prop) (hG : P G) :
         <| edgeFinset_ssubset_edgeFinset.2 <| compl_lt_compl_iff_lt.2 h) hd1) hd2
     use e, hd1.le.trans hle
 
+end SimpleGraph
