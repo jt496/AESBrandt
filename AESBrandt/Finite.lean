5347582baf7d16ed1a9e59ac12a3d720602c40c6
jt496 <john.talbot@gmail.com>
2025-02-22 01:49:46 +0000

    simpler(?) proof of exists maximal supergraph

--- AESBrandt/Finite.lean
+++ AESBrandt/Finite.lean
@@ -8,6 +8,14 @@ variable [Fintype V]
 for which `P H` holds. -/
 lemma exists_maximal_supergraph (P : SimpleGraph V → Prop) (hG : P G) :
     ∃ H, G ≤ H ∧ Maximal P H := by
+  have hwf : IsWellFounded (α := SimpleGraph V) (· > ·) := inferInstance
+  obtain ⟨H, hH, minH⟩ := WellFounded.has_min hwf.wf {H | G ≤ H ∧ P H } ⟨G, ⟨le_refl G, hG⟩⟩
+  use H, hH.1
+  rw [maximal_iff_forall_gt]
+  use hH.2, fun K hlt hf ↦ minH K ⟨(lt_of_le_of_lt hH.1 hlt).le, hf⟩ hlt
+
+lemma exists_maximal_supergraph' (P : SimpleGraph V → Prop) (hG : P G) :
+    ∃ H, G ≤ H ∧ Maximal P H := by
   simp_rw [maximal_iff_forall_gt]
   classical
   revert hG
@@ -20,12 +28,4 @@ lemma exists_maximal_supergraph (P : SimpleGraph V → Prop) (hG : P G) :
     obtain ⟨e, hle, he⟩:= hc d ((fun h ↦ Finset.card_lt_card
         <| edgeFinset_ssubset_edgeFinset.2 <| compl_lt_compl_iff_lt.2 h) hd1) hd2
     use e, hd1.le.trans hle
-
 end SimpleGraph
-
-lemma exists_not_lt_of_fintype  [Fintype α] [Nonempty α] [Preorder α] :
-    ∃ b : α, ∀ c : α, ¬ b < c := by
-  by_contra! h
-  let f : ℕ → α := fun n ↦ Nat.rec Classical.ofNonempty (fun n fₙ ↦ (h fₙ).choose) n
-  exact not_injective_infinite_finite f
-    (strictMono_nat_of_lt_succ (fun n ↦ (h (f n)).choose_spec)).injective
