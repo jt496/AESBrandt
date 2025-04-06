d070a7e6358cad76f2d4cc21ed7dd6d363d016a2
jt496 <john.talbot@gmail.com>
2025-04-06 10:20:27 +0100

    induced colorings

--- AESBrandt/BlowupGraph.lean
+++ AESBrandt/BlowupGraph.lean
@@ -1,16 +1,6 @@
-/-
-Copyright (c) 2024 John Talbot and Lian Bremner Tattersall. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: John Talbot, Lian Bremner Tattersall
--/
 import Mathlib.Combinatorics.SimpleGraph.Coloring
-import Mathlib.Combinatorics.SimpleGraph.Path
-import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
 
 namespace SimpleGraph
-#check SimpleGraph.completeMultipartiteGraph
-
-
 
 /-- Given a family of vertex types indexed by `ι`, pulling back from `H : SimpleGraph ι`
 yields the blow-up graph on the family. Two vertices are adjacent if and only if their
@@ -50,53 +40,5 @@ lemma blowupGraph_colorable_iff  {ι : Type*} {n : ℕ} (H : SimpleGraph ι) (V 
   constructor <;> intro ⟨c⟩
   · exact ⟨fun x ↦ c x.fst, fun h1 h2 ↦ c.valid h1 h2⟩
   · exact ⟨fun x ↦ c ⟨x, f x⟩, by intro a b had; exact c.valid (by rwa [blowupGraph_adj])⟩
-
-open ConnectedComponent Subgraph
-
-def coloringOfComponents {α β: Type*} {G : SimpleGraph α}
-    (h : ∀ (c : G.ConnectedComponent), (G.induce c.supp).Coloring β) :
-    G.Coloring β := by
-  exact ⟨fun v ↦ h (G.connectedComponentMk v) ⟨v, rfl⟩, by
-    simp only [top_adj]
-    intro a b hab heq
-    have := connectedComponentMk_eq_of_adj hab
-    have hadj : (G.induce (G.connectedComponentMk a).supp).Adj ⟨a, rfl⟩
-       ⟨b, ((G.connectedComponentMk a).mem_supp_congr_adj hab).1 rfl⟩ := by simpa using hab
-    exact (h _).valid hadj (by convert heq)⟩
-
-variable {α : Type*} {n : ℕ} {G : SimpleGraph α}
-theorem colorable_iff_forall_connectedComponents  :
-    G.Colorable n ↔ ∀ c : G.ConnectedComponent, (G.induce c.supp).Colorable n :=
-  ⟨fun ⟨C⟩ _ ↦ ⟨fun v ↦ C v.1, fun h h1 ↦ C.valid h h1⟩,
-     fun h ↦ ⟨coloringOfComponents (fun c ↦ (h c).some)⟩⟩
-
-/-- `G` is `n`-colorable if all its induced connected subgraphs are `n`-colorable-/
-theorem colorable_iff_induced_connected :
-    (∀ s, (G.induce s).Connected → (G.induce s).Colorable n) ↔ G.Colorable n := by
-  constructor <;> intro h
-  · rw [colorable_iff_forall_connectedComponents]
-    intro c
-    apply h
-    rw [connected_induce_iff, connected_iff_forall_exists_walk_subgraph]
-    refine ⟨c.nonempty_supp,?_⟩
-    intro u v hu hv
-    simp_rw [induce_verts, mem_supp_iff] at hu hv
-    have : G.connectedComponentMk u = G.connectedComponentMk v := by
-      rw [hv, hu]
-    obtain ⟨w⟩ := ConnectedComponent.exact this
-    use w
-    induction w with
-    | nil => simpa
-    | cons h p ih =>
-      simp_rw [Walk.toSubgraph, sup_le_iff]
-      constructor
-      · apply subgraphOfAdj_le_of_adj
-        simpa using ⟨hu, hu.symm ▸ connectedComponentMk_eq_of_adj h.symm, h⟩
-      · exact ih (hu.symm ▸ connectedComponentMk_eq_of_adj h.symm)
-                hv (ConnectedComponent.sound ⟨p⟩)
-  · intro s _
-    obtain ⟨C⟩ := h
-    exact ⟨fun v ↦ (C v.1), fun a ↦ Hom.map_adj C a⟩
-
 
 end SimpleGraph
