b82ffe1861c9c79aa7e42f807e72e792ffa3258b
jt496 <john.talbot@gmail.com>
2025-05-20 13:33:35 +0100

    minDegree_le_of_cliqueFree_FiveWheelLikeFree_succ

--- AESBrandt/FiveWheelLike.lean
+++ AESBrandt/FiveWheelLike.lean
@@ -6,6 +6,8 @@ Authors: John Talbot, Lian Bremner Tattersall
 import Mathlib.Combinatorics.SimpleGraph.CompleteMultipartite
 import Mathlib.Algebra.BigOperators.Ring.Finset
 import Mathlib.Algebra.Order.BigOperators.Group.Finset
+import Mathlib.Tactic.Linarith
+import Mathlib.Tactic.Ring
 /-!
 If `G` is maximally `Kᵣ₊₂`-free and `¬ G.Adj x y` (with `x ≠ y`) then there exists an `r`-set `s`
  such that `s ∪ {x}` and `s ∪ {y}` are both `r + 1`-cliques.
@@ -24,13 +26,23 @@ Main definition:
 * `SimpleGraph.IsFiveWheelLike`: predicate for `v w₁ w₂ s₁ s₂` to form a 5-wheel-like subgraph of
  `G` with `r`-sets `s₁` and `s₂`, and vertices `v w₁ w₂` forming an `IsPathGraph3Compl`. -/
 
+local notation "‖" x "‖" => Fintype.card x
+
 open Finset SimpleGraph
 
 variable {α : Type*} {a b c d x y : α} {s : Finset α} {G : SimpleGraph α} {r k : ℕ}
 
 section Counting
 
-variable {i j : ℕ} [DecidableRel G.Adj] --{α : Type*} {G : SimpleGraph α}  {x : α} {s : Finset α}
+variable {i j n : ℕ} [DecidableRel G.Adj] --{α : Type*} {G : SimpleGraph α}  {x : α} {s : Finset α}
+
+private lemma kr_bound (hk : k ≤ r) :
+    (2 * r + 2 + k) * n / (2 * r + 2 + k + 3) ≤ (3 * r + 2) * n / (3 * r + 5) := by
+  apply (Nat.le_div_iff_mul_le <| Nat.succ_pos _).2
+      <| (mul_le_mul_left (2 * r + 2 + k + 2).succ_pos).1 _
+  rw [← mul_assoc, mul_comm (2 * r + 2 + k + 3), mul_comm _ (_ * n)]
+  apply (Nat.mul_le_mul_right _ (Nat.div_mul_le_self ..)).trans
+  nlinarith
 
 /-- Transform lower bound on non-adjacency into upper bound on adjacency -/
 private lemma card_adj_le_of_le_card_not_adj (hx : i ≤ #(s.filter fun z ↦ ¬ G.Adj x z)) :
@@ -191,10 +203,10 @@ lemma exist_not_adj_of_adj_inter (h : G.CliqueFree (r + 2)) (hW : ∀ {y}, y ∈
     · exact hbj <| hW <| mem_inter_of_mem hbs hb
 
 lemma card_add_card_inter :
-    #(insert v <| insert w₁ <| insert w₂ (s₁ ∪ s₂)) + #(s₁ ∩ s₂) = 2 * r + 3 := by
+    #(insert v <| insert w₁ <| insert w₂ (s₁ ∪ s₂)) + k = 2 * r + 3 := by
   rw [add_comm, card_insert_of_not_mem, card_insert_of_not_mem, card_insert_of_not_mem]
-  · simp_rw [← add_assoc, card_inter_add_card_union, two_mul, hw.card_isNClique_erase,
-    hw.symm.card_isNClique_erase]
+  · simp_rw [← add_assoc, ← hw.card_eq, card_inter_add_card_union, two_mul,
+            hw.card_isNClique_erase, hw.symm.card_isNClique_erase]
   · rw [mem_union, not_or]
     exact ⟨hw.symm.fst_not_mem_snd, hw.snd_not_mem⟩
   · rw [mem_insert, mem_union, not_or, not_or]
@@ -309,13 +321,78 @@ If G is Kᵣ₊₂-free and contains an `IsFiveWheelLike r k` structure but
 `G.FiveWheelLikeFree r (k + 1)` then every vertex that is adjacent to all of the common clique
 vertices is non-adjacent to at least 3 vertices of the wheel.
 -/
-lemma three_le_not_adj_of_cliqueFree_FiveWheelLikeFree (hcf : G.CliqueFree (r + 2))
-    (hW : ∀ {y}, y ∈ s₁ ∩ s₂ → G.Adj x y) (hmax :  G.FiveWheelLikeFree r (k + 1)) :
+lemma three_le_not_adj_of_cliqueFree_FiveWheelLikeFree_succ (hcf : G.CliqueFree (r + 2))
+    (hW : ∀ {y}, y ∈ s₁ ∩ s₂ → G.Adj x y) (hmax : G.FiveWheelLikeFree r (k + 1)) :
     3 ≤ #(({v} ∪ ({w₁} ∪ ({w₂} ∪ (s₁ ∪ s₂)))).filter fun z ↦ ¬ G.Adj x z) := by
   by_contra! hc
   obtain ⟨_, _, _, _, hbW⟩ := hw.exists_isFiveWheelLike_insert_of_not_adj_le_two hcf hW
                                         <| Nat.succ_le_succ_iff.1 hc
   exact hmax hbW
+
+lemma minDegree_le_of_cliqueFree_FiveWheelLikeFree_succ [Fintype α] (hcf : G.CliqueFree (r + 2))
+    (hm : G.FiveWheelLikeFree r (k + 1)) : G.minDegree ≤ (2 * r + k) * ‖α‖ / (2 * r + k + 3) := by
+  let X := {x | ∀ {y}, y ∈ s₁ ∩ s₂ → G.Adj x y}.toFinset
+-- and `W` which consists of all vertices of the 5-wheel.
+  let W := insert v <| insert w₁ <| insert w₂ (s₁ ∪ s₂)
+-- Any vertex in `X` has at least 3 non-neighbors in `W` (otherwise we can build a bigger wheel)
+  have dXle : ∀ x, x ∈ X → 3 ≤ #(W.filter fun z ↦ ¬ G.Adj  x z) := by
+    intro z hx
+    simp_rw [X, Set.toFinset_setOf, mem_filter, mem_univ, true_and] at hx
+    exact hw.three_le_not_adj_of_cliqueFree_FiveWheelLikeFree_succ hcf hx hm
+-- Every vertex has at least 1 non-neighbor in `W`.
+-- So we have a bound on the degree sum over `W`
+-- `∑ w ∈ W, H.degree w ≤  |X| * (|W| - 3) + |Xᶜ| * (|W| - 1)`
+  have bdW := sum_degree_le_of_le_not_adj dXle (fun y _ ↦ (hw.one_le_not_adj_of_cliqueFree hcf) y)
+-- Since `X` consists of all vertices adjacent to all of `s₁ ∩ s₂`,
+-- so any `x ∈ Xᶜ` has at least one non-neighbour in `X`.
+  have xcle : ∀ x, x ∈ Xᶜ → 1 ≤ #((s₁ ∩ s₂).filter fun z ↦ ¬ G.Adj x z) := by
+    intro x hx
+    apply card_pos.2
+    obtain ⟨_, hy⟩ : ∃ y ∈ s₁ ∩ s₂, ¬ G.Adj x y := by
+      contrapose! hx
+      rw [mem_compl, not_not, Set.mem_toFinset]
+      exact hx _
+    exact ⟨_, mem_filter.2 hy⟩
+-- So we also have a bound on the degree sum over `s₁ ∩ s₂`
+-- `∑ w ∈ s₁ ∩ s₂, H.degree w ≤  |Xᶜ| * (|s₁ ∩ s₂| - 1) + |X| * |s₁ ∩ s₂|`
+  have bdX := sum_degree_le_of_le_not_adj xcle (fun x _ ↦ Nat.zero_le _)
+  rw [compl_compl, tsub_zero, add_comm] at bdX
+  -- cases r with
+  -- | zero   => exact (Nat.not_succ_le_zero _ <| hw.card_inter_lt_of_cliqueFree hcf).elim
+  -- | succ r =>
+  rw [Nat.le_div_iff_mul_le (Nat.add_pos_right _ zero_lt_three)]
+  have Wc : #W + k = 2 * r + 3 := hw.card_add_card_inter
+  have w3 : 3 ≤ #W := hw.three_le_card
+--- Two cases: `s₁ ∩ s₂ = ∅`
+  by_cases hst : k = 0
+  · rw [hst, add_zero] at Wc ⊢
+    rw [← Wc, ← tsub_eq_of_eq_add Wc]
+    have Xu : X = univ := by
+      apply eq_univ_of_forall
+      rw [← hw.card_eq, card_eq_zero] at hst
+      intro x; simp [X, hst]
+    rw [Xu, card_univ, compl_univ, card_empty, zero_mul, add_zero, mul_comm] at bdW
+    apply bdW.trans'
+    rw [card_eq_sum_ones, mul_sum, mul_one]
+    exact sum_le_sum (fun i _ ↦ G.minDegree_le_degree i)
+--- `s₁ ∩ s₂ ≠ ∅`
+  · have hap :  #W - 1 + 2 * (k - 1) = #W - 3 + 2 * k := by omega
+    calc
+    minDegree G * (2 * r + k + 3) ≤ ∑ w ∈ W, G.degree w +  2 * ∑ w ∈ s₁ ∩ s₂, G.degree w := by
+        rw [add_assoc, add_comm k, ← add_assoc, ← Wc, add_assoc, ← two_mul, mul_add]
+        simp_rw [← hw.card_eq, card_eq_sum_ones, ← mul_assoc, mul_sum, mul_one]
+        apply add_le_add <;> apply sum_le_sum <;> intro i _
+        · exact minDegree_le_degree ..
+        · exact mul_comm 2 _ ▸ (Nat.mul_le_mul_left _ <| G.minDegree_le_degree _)
+    _ ≤ #X * (#W - 3) + #Xᶜ * (#W - 1) + 2 * (#X * k + #Xᶜ * (k - 1)) :=
+          add_le_add bdW <| Nat.mul_le_mul_left _ (hw.card_eq ▸ bdX)
+    _ = #X * (#W - 3 + 2 * k) + #Xᶜ * ((#W - 1) + 2 * (k - 1)) := by ring_nf
+    _ ≤ (2 * r + k) * ‖α‖ := by
+        rw [hap, ← add_mul, card_compl, add_tsub_cancel_of_le (card_le_univ _), mul_comm]
+        apply Nat.mul_le_mul_right
+        rw [two_mul, ← add_assoc]
+        apply Nat.add_le_add_right
+        rw [tsub_add_eq_add_tsub w3, Wc, Nat.add_sub_cancel_right]
 
 end IsFiveWheelLike
 
